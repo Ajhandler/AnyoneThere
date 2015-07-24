6ead4b7553296c6ce0525dc59faf3e56dbf2b41b
Alexander Handler <Alexjhandler@gmail.com>
2015-07-24 15:21:17 -0400

    add username to users, username displays with posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724064625) do
+ActiveRecord::Schema.define(version: 20150724190532) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(version: 20150724064625) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
